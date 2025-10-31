585f69a31e0697a4bccec1e19e1456f4eccbdb62
kuuhaku1102 <j-@infintydesign.online>
2025-10-31 19:32:14 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<h2 class="section-title">最新の記事</h2>
+<h2 class="section-title">テスト最新の記事</h2>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <div class="card">
     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -10,3 +10,4 @@
   <p>記事がありません。</p>
 <?php endif; ?>
 <?php get_footer(); ?>
+
